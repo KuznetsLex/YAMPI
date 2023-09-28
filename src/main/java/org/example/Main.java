eff1ad56c2ac619cfd06630731f54e65d4638b31
KuznetsLex <kuznets.lex@gmail.com>
2023-09-28 17:14:01 +0600

    Init master branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,7 @@
 package org.example;
 public class Main {
     public static void main(String[] args){
-//        Работа со строками
-        String s1 = "ало ";
-        System.out.println(StringProcessor.copy(s1,3));
-        System.out.println(StringProcessor.insertionsCount("abaa", "aa"));
-        String s2 = "1 баран, 2 барана, 3 барана, 4 барана";
-        System.out.println(StringProcessor.numbersToText(s2));
-        StringBuilder s3 = new StringBuilder("123456789");
-        System.out.println(StringProcessor.deleteEverySecond(s3));
+
     }
 }
 
